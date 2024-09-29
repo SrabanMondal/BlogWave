5cb7849cb67285afadbe0faa4a050d4f7e14d329
SrabanMondal <152634742+SrabanMondal@users.noreply.github.com>
2024-09-29 12:30:32 +0530

    Update SearchName.tsx

--- src/components/SearchName.tsx
+++ src/components/SearchName.tsx
@@ -15,10 +15,10 @@ const SearchName:React.FC<SearchNameProps> = ({setblogs,toggle}) => {
         }
     }
   return (
-    <Flex gap={4} direction={'column'}>
+    <Flex gap={4} direction={'column'} color={'#dcdcdc'}>
         <Heading fontWeight={700} color='#d3d3d3'>Search by Name</Heading>
         <Flex gap={3}>
-        <Input value={name} onChange={(e)=>setname(e.target.value)}/>
+        <Input color={'#d3d3d3'} value={name} onChange={(e)=>setname(e.target.value)}/>
         <Button px={8} py={2} leftIcon={<SearchIcon/>} onClick={handleSearch}>Search</Button>
         <Tooltip label='Search by Tags' aria-label='a tooltip'>
         <IconButton aria-label='swap' icon={<RepeatIcon/>} onClick={()=>toggle(prev=>!prev)}/>
